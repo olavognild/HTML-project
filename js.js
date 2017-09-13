ebcfee814dd3821d5bca497aa3a2c2e502b20ee9
Timmyeriksson <timmyerikericsson@gmail.com>
2017-09-13 15:05:13 +0200

    Slide om oss

--- js.js
+++ js.js
@@ -6,3 +6,14 @@ $(document).scroll(function(){
         $("nav, .plogo").removeClass("scroll");
     }
 });
+
+
+function bytSlide() {
+    var slide1 = document.getElementById("personSlide1"); 
+    var slide2 = document.getElementById("personSlide2");
+ 
+    slide1.style.display = (
+        slide1.style.display == "none" ? "block" : "none"); 
+    slide2.style.display = (
+        slide2.style.display == "none" ? "block" : "none"); 
+ }
