6bfd102b6fcfaa395659d55dd59cff18c0516273
Timmyeriksson <timmyerikericsson@gmail.com>
2017-09-22 15:06:07 +0200

    VG-krav + valideringsfix

--- js.js
+++ js.js
@@ -59,7 +59,11 @@ $(document).ready(function () {
         $('body').animate({ scrollTop: $path }, 1000);
     })
 
-    fetch('https://api.github.com/search/repositories?q=language:javascript+created:>2017-09-13&sort=stars&order=desc')
+    var datum = new Date();
+    datum.setDate(datum.getDate() - 7);
+    var tidigareDatum = datum.toISOString().slice(0, 10);
+
+    fetch('https://api.github.com/search/repositories?q=language:javascript+created:>' + tidigareDatum + '&sort=stars&order=desc')
         .then(response => response.json())
         .then(data => {
             for (var i = 0; i < 5; i++) {
@@ -68,11 +72,24 @@ $(document).ready(function () {
                 var projektnamn = data.items[i].name;
                 var linkägare = data.items[i].owner.html_url;
                 var anvnamn = data.items[i].owner.login;
-                $('#jstrend').append(" - Namn: " + "<a target=" + "'_blank'" + "href='" + linknamn + "'>" + "<strong>" + projektnamn + "</strong>" + "</a>" + " | Ägare: " + "<a target=" + "'_blank'" + "href='" + linkägare + "'>" + "<strong>" + anvnamn + "</strong>" + "</a>" + " | Stjärnor: " + item.stargazers_count + "<br>");
+                var stjarnor = data.items[i].stargazers_count;
+                var collnamn = data.items[i].contributors_url;
+                $('#jstrend').append(" - Namn: " + "<a target=" + "'_blank'" + "href='" + linknamn + "'>" + "<strong>" + projektnamn + "</strong>" + "</a>" + " | Ägare: " + "<a target=" + "'_blank'" + "href='" + linkägare + "'>" + "<strong>" + anvnamn + "</strong>" + "</a>" + " | Stjärnor: " + stjarnor + "<br>");
+                fetch(collnamn)
+                    .then(response => response.json())
+                    .then(data => {
+                        for (var z = 0; z < data.length; z++) {
+                            var objekt = data[z];
+                            var contribnamn = data[z].login;
+                            var contribavatar = data[z].avatar_url;
+                            $('#JScontributors').append("Contributor: " + contribnamn + "<br> <img src='" + contribavatar + "' alt='avatarbild' class='avatarbilder'><br>");
+                        }
+                    })
             }
         });
 
-    fetch('https://api.github.com/search/repositories?q=language:css+created:>2017-09-13&sort=stars&order=desc')
+
+    fetch('https://api.github.com/search/repositories?q=language:css+created:>' + tidigareDatum + '&sort=stars&order=desc')
         .then(response => response.json())
         .then(data => {
             for (var i = 0; i < 5; i++) {
@@ -81,10 +98,41 @@ $(document).ready(function () {
                 var projektnamn = data.items[i].name;
                 var linkägare = data.items[i].owner.html_url;
                 var anvnamn = data.items[i].owner.login;
-                $('#csstrend').append(" - Namn: " + "<a target=" + "'_blank'" + "href='" + linknamn + "'>" + "<strong>" + projektnamn + "</strong>" + "</a>" + " | Ägare: " + "<a target=" + "'_blank'" + "href='" + linkägare + "'>" + "<strong>" + anvnamn + "</strong>" + "</a>" + " | Stjärnor: " + item.stargazers_count + "<br>");
+                var stjarnor = data.items[i].stargazers_count;
+                var collnamn = data.items[i].contributors_url;
+                $('#csstrend').append(" - Namn: " + "<a target=" + "'_blank'" + "href='" + linknamn + "'>" + "<strong>" + projektnamn + "</strong>" + "</a>" + " | Ägare: " + "<a target=" + "'_blank'" + "href='" + linkägare + "'>" + "<strong>" + anvnamn + "</strong>" + "</a>" + " | Stjärnor: " + stjarnor + "<br>");
+                fetch(collnamn)
+                    .then(response => response.json())
+                    .then(data => {
+                        for (var z = 0; z < data.length; z++) {
+                            var objekt = data[z];
+                            var contribnamn = data[z].login;
+                            var contribavatar = data[z].avatar_url;
+                            $('#CSScontributors').append("Contributor: " + contribnamn + "<br> <img src='" + contribavatar + "' alt='avatarbild' class='avatarbilder'><br>");
+                        }
+                    })
             }
-        });
-})
+        })
+    });
+    
+
+function visaMerJavascript() {
+    var divJS = document.getElementById("JScontributors");
+    if (divJS.style.display === "block") {
+        divJS.style.display = "none";
+    } else {
+        divJS.style.display = "block";
+    }
+}
+
+function visaMerCSS() {
+    var divCSS = document.getElementById("CSScontributors");
+    if (divCSS.style.display === "block") {
+        divCSS.style.display = "none";
+    } else {
+        divCSS.style.display = "block";
+    }
+}
 
 
 //Bildspel
@@ -216,3 +264,30 @@ $(document).ready(function () {
 
     });
 });
+
+function sparaData() {
+    var namnet = document.getElementById("namn");
+    var namnfärg = namnet.style.backgroundColor;
+    var mailen = document.getElementById("mail");
+    var mailfärg = mailen.style.backgroundColor;
+    var telefonen = document.getElementById("tlf");
+    var telefonfärg = telefonen.style.backgroundColor;
+    var meddelande = document.getElementById("contactMsg");
+    var meddelandefärg = meddelande.style.backgroundColor;
+    var sparadeDatan = {
+        namn: document.getElementById("namn").value,
+        telefon: document.getElementById("tlf").value,
+        mail: document.getElementById("mail").value
+    }
+    if (namnfärg === "green" || "white" && telefonfärg === "green" || "white" && mailfärg === "green" || "white" && meddelandefärg === "green") {
+        alert("Skickat!");
+        window.localStorage.setItem("data", JSON.stringify(sparadeDatan));
+    } else {
+        alert("Något är tokigt");
+    }
+}
+var lagradData = JSON.parse(window.localStorage.getItem("data"));
+
+document.getElementById("namn").value = lagradData.namn;
+document.getElementById("tlf").value = lagradData.telefon;
+document.getElementById("mail").value = lagradData.mail;
